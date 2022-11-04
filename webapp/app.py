3cd1073f64f3e475743831116ff05359c0ba42ce
sabmohmayahaii1212 <66863807+sabmohmayahaii1212@users.noreply.github.com>
2022-11-05 02:40:33 +0530

    Add files via upload

---
+++ webapp/app.py
@@ -0,0 +1,25 @@
+
+import flask
+import pickle
+# Use pickle to load in the pre-trained model.
+with open(f'../webapp/model/crop_data.pkl', 'rb') as f:
+    model = pickle.load(f)
+app = flask.Flask(__name__, template_folder='templates')
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    if flask.request.method == 'POST':
+        average_rain_fall_mm_per_year = flask.request.form['average_rain_fall_mm_per_year']
+        pesticides_tonnes = flask.request.form['pesticides_tonnes']
+        avg_temp = flask.request.form['avg_temp']
+        input_variables = pd.DataFrame([[average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]],
+                                       columns=['average_rain_fall_mm_per_year', 'pesticides_tonnes', 'avg_temp'],
+                                       dtype=float)
+        prediction = model.predict(input_variables)[0]
+        return flask.render_template('main.html',
+                                     original_input={'Average_rain_fall_mm_per_year':average_rain_fall_mm_per_year,
+                                                     'Pesticides_tonnes':pesticides_tonnes,
+                                                     'Avg_temp':avg_temp},
+                                     result=prediction,
+                                     )

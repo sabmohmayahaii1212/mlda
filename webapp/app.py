8e6ff3d8123e5dd162771fc523e01bd081403cd6
sabmohmayahaii1212 <66863807+sabmohmayahaii1212@users.noreply.github.com>
2022-11-05 04:00:36 +0530

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -25,6 +25,4 @@ def main():
                                                      'Avg_temp':avg_temp},
                                      result=prediction,
                                      )
-    if __name__=="__main__":
-    port = os.environ.get("PORT", 5000)
-    app.run(debug=False,host='0.0.0.0', port=port)
+   

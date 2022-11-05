3ee8b8cd98988b8cad9efaadc4cc6a6977e3241c
sabmohmayahaii1212 <66863807+sabmohmayahaii1212@users.noreply.github.com>
2022-11-05 08:11:46 +0530

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -27,7 +27,7 @@ def main():
                                      )
        if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
+        port = int(os.environ.get('PORT', 5000))
     
-    app.run(host='127.0.0.1', port=port)
+        app.run(host='0.0.0.0', port=port)
    
